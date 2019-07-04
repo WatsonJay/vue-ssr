4dce4bdc0283a1723937a4525eca63beeac36ea2
WatsonJay <446725026@qq.com>
2019-07-04 21:54:43 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,32 @@
 <template>
-    <div id="test">{{text}}</div>
+    <div id="app">
+        <div id="cover">
+
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "app",
-        data(){
-            return{
-                text: 'hello world'
-            }
-        }
     }
 </script>
 
-<style>
-    #test{
-        color: red;
-    }
+<style lang="stylus" scoped>
+#app{
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+}
+#cover{
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background-color #999
+    opacity .9
+    z-index -1
+}
 </style>
