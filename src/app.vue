1bf4fc06507b85ca172be79cabf32af4aa477418
WatsonJay <446725026@qq.com>
2019-07-04 20:45:25 +0800

    init webpack

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="test">{{text}}</div>
+</template>
+
+<script>
+    export default {
+        name: "app",
+        data(){
+            return{
+                text: 'hello world'
+            }
+        }
+    }
+</script>
+
+<style>
+    #test{
+        color: red;
+    }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+const isDev = process.env.NODE_ENV === 'development'
+const  HTMLPlugin = require('html-webpack-plugin')
+
+const config = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, 'dist')
+    },
+    //. 参考官方文档 https://vue-loader.vuejs.org/migrating.html#a-plugin-is-now-required
+    // . Vue-loader在15.*之后的版本都是 vue-loader的使用都是需要伴生 VueLoaderPlugin的,
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV: isDev ? '"development"' : '"production"'  //避免转义成变量
+            }
+        }),
+        new HTMLPlugin(),
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name]-aaa.[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+if (isDev){
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay:{
+            errors: true,
+        },
+        hot: true
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config
